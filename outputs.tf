30818e44c08ed028cc196d8c650ac956ba96fb4c
The-Lorax-Club <dcnetworktech@gmail.com>
2025-08-06 22:31:03 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,24 @@
-#output "instance_ami" {
-#  value = aws_instance.web.ami
-#}
+output "instance_main_ip" {
+  value       = vultr_instance.web.main_ip
+  description = "The main IPv4 address of the Vultr instance"
+}
 
-#output "instance_arn" {
-#  value = aws_instance.web.arn
-#}
+output "instance_id" {
+  value       = vultr_instance.web.id
+  description = "The unique Vultr instance ID"
+}
+
+output "instance_label" {
+  value       = vultr_instance.web.label
+  description = "The label for the Vultr instance"
+}
+
+output "instance_os_id" {
+  value       = vultr_instance.web.os_id
+  description = "The OS ID used to create the Vultr instance"
+}
+
+output "instance_region" {
+  value       = vultr_instance.web.region
+  description = "The region in which the Vultr instance was deployed"
+}
