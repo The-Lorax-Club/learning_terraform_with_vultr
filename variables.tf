16c5cb62925efbf38b2f03ae54617ecca49c06e6
The-Lorax-Club <dcnetworktech@gmail.com>
2025-08-08 16:56:41 -0500

    add firewall group and rules with my ip as a variable

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "vultr_os_ubuntu_id" {
     description = "OS ID for vultr VM"
     default = "1743"
 }
+
+variable "my_public_ip" {
+    description = "My Public IP"
+    default = "71.194.141.105/32"
+}
