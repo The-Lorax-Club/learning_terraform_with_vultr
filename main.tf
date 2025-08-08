e94ae47bde762ca7bc362a0c4f60782eafd4d5eb
The-Lorax-Club <dcnetworktech@gmail.com>
2025-08-08 14:19:29 -0500

    fix variable name syntax

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "vultr_instance" "web" {
   label       = "HelloWorld"
   plan        = "vhf-1c-1gb"
   region      = "ord"
-  os_id       = data.vultr_os.ubuntu.id
+  os_id       = vultr_os-ubuntu-id
   enable_ipv6 = false
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "data.vultr_os.ubuntu.id" {
+variable "vultr_os-ubuntu-id" {
     description = "OS ID for vultr VM"
     default = "1743"
 }
