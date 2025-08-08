63ae79bc022952cd23e25841b66f93853869367f
The-Lorax-Club <dcnetworktech@gmail.com>
2025-08-07 20:28:37 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,7 @@
-data "vultr_os" "ubuntu" {
-  filter {
-    name   = "name"
-    values = ["Ubuntu 24.04 x64"]
-  }
-}
-
 resource "vultr_instance" "web" {
   label       = "HelloWorld"
   plan        = "vhf-1c-1gb"
   region      = "chi"
-  os_id       = data.vultr_os.ubuntu.id
+  os_id       = 1743
   enable_ipv6 = false
 }
